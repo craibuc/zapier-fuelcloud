f4b6ef18dd657f8e66cca81cc9a3d88bf464cff9
Craig Buchanan <craig.buchanan@cogniza.com>
2023-07-29 14:42:32 +0000

    adding authentication

---
+++ authentication.js
@@ -0,0 +1,73 @@
+const perform = async (z, bundle) => {
+
+  const options = {
+    url: 'https://store.zapier.com/api/records?key={{bundle.authData.storage_key}}',
+    method: 'GET',
+    headers: {
+      'accept': 'application/json',
+      'X-Secret': bundle.authData.storage_secret
+    },
+  }
+  // z.console.log('options',options)
+
+  return z.request(options).then((response) => {
+
+    const results = response.json;
+    // z.console.log('results',results)
+
+    bundle.authData.access_token = bundle.authData.storage_key
+
+    return {
+      'sessionKey': results[bundle.authData.storage_key]
+    };
+
+  });
+
+};
+
+const test = async (z, bundle) => {
+
+  const options = {
+    url: 'https://api.fuelcloud.com/rest/v1.0/timezones',
+    method: 'GET',
+    headers: {
+      Authorization: '{{bundle.authData.access_token}}',
+    },
+  };
+
+  return z.request(options).then((response) => {
+
+    const results = response.json;
+    // z.console.log('results',results)
+    return results.data;
+
+  });
+
+};
+
+module.exports = {
+  type: 'session',
+  sessionConfig: {
+    perform: perform,
+  },
+  test: test,
+  fields: [
+    {
+      key: 'storage_secret',
+      label: 'Storage Secret',
+      type: 'password',
+      required: true,
+      helpText: "The secret that was used when creating the 'Storage by Zapier' connection.",
+      computed: false,
+    },
+    {
+      key: 'storage_key',
+      label: 'Storage Key',
+      type: 'string',
+      required: true,
+      helpText: 'The name of the storage key that contains the FuelCloud Access Token.',
+      computed: false,
+    },
+  ],
+  connectionLabel: '',
+};
---
+++ test/authentication.test.js
@@ -0,0 +1,123 @@
+/*
+.Example
+npm test -- authentication
+
+run the tests in this file.
+
+.Example
+nvm exec v18 npm test -- authentication
+
+run the tests in this file using nvm (node version manager)
+*/
+
+const zapier = require('zapier-platform-core');
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
+
+// load .env
+zapier.tools.env.inject();
+
+describe('authentication', () => {
+  
+  describe('perform', () => {
+
+    // arrange
+    let bundle = {
+      authData: {
+        storage_secret: process.env.STORAGE_SECRET,
+        storage_key: process.env.STORAGE_KEY,
+      },
+    };
+
+    describe('when a valid Storage Secret and Storage Key are supplied', () => {
+
+      it('it returns a sessionKey', async () => {
+    
+        // act
+        const result = await appTester(
+          App.authentication.sessionConfig.perform,
+          bundle
+        );
+  
+        // assert
+        expect(result).toHaveProperty('sessionKey');
+        expect(bundle.authData.access_token).not.toBeNull();
+    
+      });
+
+    });
+
+    describe('when a invalid Storage Secret and Storage Key are supplied', () => {
+
+      it('throws an error', async () => {
+
+        // arrange
+        bundle = {
+          authData: {
+            storage_secret: 'Invalid Secret',
+            storage_key: 'Invalid Key',
+          }
+        };
+      
+        // act/assert
+        await expect(appTester(
+            App.authentication.sessionConfig.perform,
+            bundle
+        )).rejects.toThrow();
+
+      });
+  
+    });
+
+  });
+
+  describe('test', () => {
+  
+    // arrange
+    let bundle = {
+      authData: {
+        access_token: process.env.ACCESS_TOKEN,
+      },
+    };
+
+    describe('when the Access Token is valid', () => {
+
+      it('returns a list of timezones', async () => {
+    
+        // act
+        const results = await appTester(
+          App.authentication.test,
+          bundle
+        );
+  
+        // assert
+        expect(results.length).toBeGreaterThan(0)
+    
+      });
+  
+    });
+
+    describe('when the Access Token is invalid', () => {
+
+      it('throws an error', async () => {
+
+        // arrange
+        bundle = {
+          authData: {
+            access_token: 'Invalid Token',
+          }
+        };
+      
+        // act/assert
+        await expect(appTester(
+            App.authentication.test,
+            bundle
+        )).rejects.toThrow();
+
+      });
+  
+    });
+
+  });
+
+});
